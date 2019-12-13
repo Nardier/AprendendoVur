91213269dbbb3921c9a3d1079ef5c63cbcd0ee25
Nardier <nardierbarbosa@gmail.com>
2019-12-13 16:59:02 -0300

    incrementando gráficos

---
+++ src/components/Grafico.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <!-- <GChart
+        type="LineChart"
+        :data="chartData"
+        :options="chartOptions"
+      /> -->
+    </div>
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+export default {
+  components: {
+    GChart
+  },
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,43 +2,103 @@
   <div id="app">
     <button @click="disconnect" v-if="status === 'connected'">Disconnect</button>
     <button @click="connect" v-if="status === 'disconnected'">Connect</button> {{ status }}
-    <br /><br />
-    <div v-if="status === 'connected'">
-      <form @submit.prevent="sendMessage" action="#">
-        <input v-model="message"><button type="submit">Send Message</button>
-      </form>
-      <ul id="logs">
-        <li v-for="log in logs" class="log">
-          {{ log.event }}: {{ log.data }}
-        </li>
-      </ul>
+    <br />
+    <div>
+      <GChart
+        type="LineChart"
+        :data="chartLineData"
+        :options="chartLineOptions"
+      />
     </div>
+        <div>
+      <GChart
+        type="BarChart"
+        :data="chartBarData"
+        :options="chartBarOptions"
+      />
+    </div>
+    <br />  
   </div>
 </template>
 
 <script>
+
+import Vue from 'vue'
+import Grafico from './Grafico'
+import { GChart } from 'vue-google-charts'
+
+
 export default {
   name: 'Home',
+  components:{
+    Grafico,
+    GChart
+
+  },
   data () {
     return {
       message: "",
       logs: [],
-      status: "disconnected"
+      status: "disconnected",
+      chartLineData: [
+        ["Tempo", "Resposta"],
+        ["", 0],
+      ],
+      chartLineOptions: {   
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          title: 'Teste Latencia',
+        
+      },
+      chartBarData: [
+        ["Tempo", "Codigo"],
+        ["200", 55],
+        ["207", 2],
+      ],
+      chartBarOptions: {   
+          curveType: 'function',
+          orientation: 'horizontal',
+          legend: { position: 'bottom' },
+          title: 'Status Code',
+        
+      }
     }
+    
   },
+  ResponseStatsModel(){
+    Response_Body
+    Response_Time
+    Response_StatusCode
+    Response_Status
+    TEOM
+    Request_Url
+    },
+
   methods: {
     connect() {
-      this.socket = new WebSocket("ws://localhost:8080/stats");
+      this.socket = new WebSocket("ws://localhost:8081/stats");
       this.socket.onopen = () => {
         this.status = "connected";
         this.logs.push({ event: "Connected to", data: 'wss://localhost:8080'})
-        
+        this.socket.onmessage = ({data}) => {  
+          
+          var parsed = JSON.parse(data)
 
-        this.socket.onmessage = ({data}) => {
-          this.logs.push({ event: "Recieved message", data });
+          this.chartBarData.forEach((element) => {
+            element[0] 
+            
+          });
+
+          if(this.chartLineData.length <= 10 ){
+            this.chartLineData.push([parsed.TEOM + ' sec' , parsed.Response_Time]);            
+          }else{
+            this.chartLineData.splice(1,1)
+            this.chartLineData.push([parsed.TEOM + ' sec' , parsed.Response_Time])
+          }
+        
         };
       };
-    },
+    },  
     disconnect() {
       this.socket.close();
       this.status = "disconnected";
